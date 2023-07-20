78b3a57d10eda322241e0270dcbb14aaa4687dab
Thiagojm <tjm.plastica@gmail.com>
2023-07-20 13:12:26 -0300

    up

--- pages/1_Receitas.py
+++ pages/1_Receitas.py
@@ -8,28 +8,28 @@ import streamlit_authenticator as stauth
 
 class CustomPDF(FPDF):
     def header(self):
-        self.set_font("Helvetica", 'BI' , size = 15)
-        self.cell(200, 10, txt = "Dr. Thiago Jung Mendaçolli", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align = 'C')
-        self.cell(200, 10, txt = "CIRURGIA PLÁSTICA", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align = 'C')
+        self.set_font("Helvetica", 'BI' , size = 14)
+        self.cell(0, 10, txt = "Dr. Thiago Jung Mendaçolli", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align = 'C')
+        self.cell(0, 10, txt = "CIRURGIA PLÁSTICA", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align = 'C')
         self.cell(0, 10, txt = ("_" * 60), new_x=XPos.LMARGIN, new_y=YPos.NEXT, align = 'C')
 
     def footer(self):
         self.set_y(-40)
-        self.set_font("Helvetica", size = 12)
+        self.set_font("Helvetica", size = 10)
         self.cell(0, 10, txt = ("_" * 60), new_x=XPos.LMARGIN, new_y=YPos.NEXT, align = 'C')
         self.multi_cell(0, 10, txt = "Clínica Bioethos: Rua Padre Montoya, 300 - Centro - CEP 85851-080, Foz do Iguaçu - PR\nTel: (45) 3028-1282 - Whats: (45) 98805-0334 www.drthiagocirurgiaplastica.com.br", align = 'C')
 
 def save_pdf(filename, patient_name, document_text, document_date=None, include_date=False):
     pdf = CustomPDF(orientation="P", unit="mm", format="A4")
-    pdf.set_auto_page_break(auto=True, margin=40)
+    pdf.set_auto_page_break(auto=True, margin=30)
     pdf.add_page()
-    pdf.set_font("Helvetica", size = 15)
-    pdf.cell(0, 20, txt = "RECEITA MÉDICA", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align = 'C')
-    pdf.cell(0, 20, txt = f"Nome: {patient_name}", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align = 'L')
+    pdf.set_font("Helvetica", size = 12)
+    pdf.cell(0, 10, txt = "RECEITA MÉDICA", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align = 'C')
+    pdf.cell(0, 10, txt = f"Nome: {patient_name}", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align = 'L')
     pdf.ln(10)
     pdf.multi_cell(0, 10, txt = document_text)
     if include_date and document_date is not None:
-        pdf.ln(30)
+        pdf.ln(20)
         pdf.cell(0, 10, txt = f"{document_date.strftime('%d/%m/%Y')}", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align = 'C')
     pdf.output(filename)
 
@@ -58,7 +58,7 @@ def main():
     # add the .txt extension back onto the selected file name
     selected_file_with_ext = selected_file + '.txt'
     # read the selected file and put its contents into the 'document_text' variable
-    with open(os.path.join(receitas_folder, selected_file_with_ext), 'r') as file:
+    with open(os.path.join(receitas_folder, selected_file_with_ext), 'r', encoding="UTF-8") as file:
         document_text = file.read()
         
     patient_name = st.text_input('Nome do Paciente')
