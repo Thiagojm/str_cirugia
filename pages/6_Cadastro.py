aa4629cd4f362748e5516d3d219494dedc5ddfcd
Thiagojm <tjm.plastica@gmail.com>
2023-07-25 19:05:30 -0300

    up

--- pages/6_Cadastro.py
+++ pages/6_Cadastro.py
@@ -18,40 +18,40 @@ def delete_file(directory, filename):
     os.remove(os.path.join(directory, filename))
 
 # Create a selectbox to choose the action
-action = st.sidebar.selectbox('Choose an action', ['Edit', 'Create', 'Delete'])
+action = st.sidebar.selectbox('Choose an action', ['Editar', 'Criar', 'Deletar'])
 
 # Create a selectbox to choose the directory
 directory = st.sidebar.selectbox('Choose a directory', directories)
 
-if action == 'Edit':
+if action == 'Editar':
     # If the action is Edit, list the files in the chosen directory for editing
     files = list_files_in_directory(directory)
     if files:
-        file_to_edit = st.selectbox('Choose a file to edit', files)
+        file_to_edit = st.selectbox('Escolha um arquivo para editar', files)
         with open(os.path.join(directory, file_to_edit), 'r', encoding="UTF-8") as f:
             content = f.read()
-        updated_content = st.text_area('File content', value=content, height=600)
-        if st.button('Save changes'):
+        updated_content = st.text_area('Conteúdo do Arquivo', value=content, height=600)
+        if st.button('Salvar modificações'):
             create_file(directory, file_to_edit, updated_content)
-            st.success('File updated successfully.')
+            st.success('Arquivo editado com sucesso')
     else:
-        st.write('No files in this directory.')
+        st.write('Sem arquivos nesse diretório')
 
-elif action == 'Create':
+elif action == 'Criar':
     # If the action is Create, provide a field to enter the filename and the content
-    filename = st.text_input('Enter a filename')
-    content = st.text_area('Enter the content', height=600)
-    if st.button('Create file'):
+    filename = st.text_input('Nome do arquivo (coloque .txt no final)')
+    content = st.text_area('Conteúdo', height=600)
+    if st.button('Criar arquivo'):
         create_file(directory, filename, content)
-        st.success('File created successfully.')
+        st.success('Arquivo criado com sucesso')
 
-elif action == 'Delete':
+elif action == 'Deletar':
     # If the action is Delete, list the files in the chosen directory for deletion
     files = list_files_in_directory(directory)
     if files:
-        file_to_delete = st.selectbox('Choose a file to delete', files)
-        if st.button('Delete file'):
+        file_to_delete = st.selectbox('Selecione um arquivo para deletar', files)
+        if st.button('Deletar arquivo'):
             delete_file(directory, file_to_delete)
-            st.success('File deleted successfully.')
+            st.success('Arquivo deletado com sucesso.')
     else:
-        st.write('No files in this directory.')
+        st.write('Sem arquivos nesse diretório.')
