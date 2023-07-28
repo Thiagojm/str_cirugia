200ab344e5f368292ab69fb02b18757d5a2bc374
Thiagojm <tjm.plastica@gmail.com>
2023-07-27 23:26:20 -0300

    up

---
+++ uptdate_app.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+repos=(
+  "/home/ubuntu/str_cirurgia"
+)
+
+echo ""
+echo "Getting latest for" ${#repos[@]} "repositories using pull --rebase"
+
+for repo in "${repos[@]}"
+do
+  echo ""
+  echo "****** Getting latest for" ${repo} "******"
+  cd "${repo}"
+  git pull --rebase
+  echo "******************************************"
+  echo ""
+  echo "Restarting systemclt - input sudo password"
+  echo ""
+done
+  sudo systemctl stop str-cirurgia.service
+  sudo systemctl start str-cirurgia.service
+  sudo systemctl status str-cirurgia.service
