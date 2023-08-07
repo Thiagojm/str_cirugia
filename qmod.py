3fe65e05020e019005dc3155e1656b92a72dd482
Thiagojm <tjm.plastica@gmail.com>
2023-08-06 23:23:58 -0300

    new receitas e termos

--- qmod.py
+++ qmod.py
@@ -13,7 +13,7 @@ class SessionState:
 def show_pdf(file_path):
     with open(file_path,"rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="400" height="500" type="application/pdf"></iframe>'
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="600" height="550" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 
